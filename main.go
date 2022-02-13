2d7ca08f6e59f845bd0ca662d06d49cddf6605b0
iosyyy <626797813@qq.com>
2022-02-13 19:32:43 +0800

    #init

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	err := DB.AutoMigrate(&Phone{})
+
+	err = DB.AutoMigrate(&Usert{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	user := &Usert{Model: gorm.Model{
+		ID: 2,
+	}, Name: "jhx", Phone: Phone{
+		Number: 911,
+		Model: gorm.Model{
+			ID: 3,
+		},
+	}}
+	// fmt.Println(user)
+	DB.Create(user)
+	// will delete phone
+	DB.Select(clause.Associations).Unscoped().Delete(&Usert{Model: gorm.Model{ID: 2}})
+
+	// this will not work
+	DB.Select(clause.Associations).Unscoped().Delete(&Usert{Model: gorm.Model{ID: 2}})
 }
